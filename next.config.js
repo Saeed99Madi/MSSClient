c547b80676a91ac980622782777fc6089716fad7
Saeed99Madi <saed.zooro@gmail.com>
2023-07-20 18:34:29 +0300

    fix proxy

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextTranslate = require('next-translate');
 module.exports = {
   ...nextTranslate(),
   env: {
-    BASE_UEL: 'http://mooiesterksolar.com/api/v1/',
-    BASEE_UEL: 'http://mooiesterksolar.com/api/v1/images/',
+    BASE_UEL: 'https://mooiesterksolar.com/api/v1/',
+    BASEE_UEL: 'https://mooiesterksolar.com/api/v1/images/',
   },
 };
