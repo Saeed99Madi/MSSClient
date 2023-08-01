edf197b0cf20c7254b9c8bda234cb57ef2d9beb3
alaaahmade <alaaahmade66@gmail.com>
2023-08-01 18:41:12 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextTranslate = require('next-translate');
 module.exports = {
   ...nextTranslate(),
   env: {
-    BASE_UEL: 'https://64.227.103.130/api/v1',
-    BASEE_UEL: 'https://64.227.103.130/api/v1/images',
+    BASE_UEL: 'http://64.227.103.130/api/v1',
+    BASEE_UEL: 'http://64.227.103.130/api/v1/images',
   },
 };
