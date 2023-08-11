0693c4b17b760a46bb9aedf2cc50f95124a2eafe
alaaahmade <alaaahmade66@gmail.com>
2023-08-12 01:18:42 +0300

    fix error

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextTranslate = require('next-translate');
 module.exports = {
   ...nextTranslate(),
   env: {
-    BASE_UEL: 'http://localhost:8080/api/v1',
-    // BASE_UEL: 'https://client.mooiesterksolar.com/api/v1',
+    // BASE_UEL: 'http://localhost:8080/api/v1',
+    BASE_UEL: 'https://client.mooiesterksolar.com/api/v1',
     BASEE_UEL: 'https://mooiesterksolar.com/api/v1/images',
   },
 };
