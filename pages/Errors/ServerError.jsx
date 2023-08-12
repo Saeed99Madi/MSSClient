6028ed105db9e97ced18fb9ef7bffdd8223f3447
Saeed99Madi <saed.zooro@gmail.com>
2023-08-12 13:12:36 +0300

    fix server error page

--- pages/Errors/ServerError.jsx
+++ pages/Errors/ServerError.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import { Box } from '@mui/material';
-import { StyledButton } from './ServerError.styled.jsx';
+import { Box, styled } from '@mui/material';
 
+const StyledButton = styled('button')({
+  background: 'transparent',
+  border: '2px solid #F64252',
+  color: '#F64252',
+  padding: '5px 15px',
+  fontSize: '1.25em',
+  transition: 'all 0.15s ease',
+  borderRadius: '5px',
+  '&:hover': {
+    background: '#F64252',
+    border: '2px solid #F64252',
+    color: '#111',
+    cursor: 'pointer',
+    transform: 'scale(1.05)',
+  },
+});
 const ServerError = () => {
   const router = useRouter();
   return (
