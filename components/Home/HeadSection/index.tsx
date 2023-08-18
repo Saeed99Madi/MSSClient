da44150d53beb105c787956562cbb8fab5c42928
Saeed99Madi <saed.zooro@gmail.com>
2023-08-18 12:29:46 +0300

    fix styles

--- components/Home/HeadSection/index.tsx
+++ components/Home/HeadSection/index.tsx
@@ -10,7 +10,7 @@ import {
 const HeadSection = () => {
   return (
     <HeadContainer sx={{ margin: '20px 0 0 0' }} id="home">
-      <LogoWrapper>
+      <LogoWrapper sx={{ height: 'auto' }}>
         <Image
           src="https://res.cloudinary.com/dt0jfo5xi/image/upload/v1689850157/msr87v9ann0kh1mskum2.png"
           alt="logo"
@@ -18,7 +18,18 @@ const HeadSection = () => {
         <HeadText sx={{ margin: 0 }}>SOLAR MSS INVERTER</HeadText>
         <HeadParagraph>ENGINEERED FOR SOLAR</HeadParagraph>
       </LogoWrapper>
-      <ImageWrapper />
+      <ImageWrapper
+        sx={{
+          marginTop: { xs: '0', sm: '0', md: '0', l: '0', xl: '0' },
+          minHeight: {
+            xs: '30vh',
+            sm: '50vh',
+            md: '50vh',
+            l: '100vh',
+            xl: '100vh',
+          },
+        }}
+      />
     </HeadContainer>
   );
 };
